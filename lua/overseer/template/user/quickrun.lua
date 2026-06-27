3943206b97987513fffe4b9155f623514fe95370
umotust <oozora78952@yahoo.co.jp>
2026-06-27 11:55:01 +0900

    feat(quickrun): integrate MarkdownPreview and CellRun
    - Open MarkdownPreview for Markdown files
    - Delegate Python execution to CellRun when available
    - Move quickfix window to the right and resize automatically
    - Use `python` instead of `python3`

--- lua/overseer/template/user/quickrun.lua
+++ lua/overseer/template/user/quickrun.lua
@@ -2,15 +2,37 @@
 local files = require("overseer.files")
 
 return {
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "qf",
+    callback = function()
+      vim.cmd("wincmd L")
+      vim.cmd("vertical resize " .. math.floor(vim.o.columns * 0.5))
+    end,
+  }),
+
   generator = function(_, cb)
     local file = vim.fn.expand("%:p")
     local ft = vim.bo.filetype
+
+    if ft == "markdown" and vim.fn.exists(":MarkdownPreview") == 2 then
+      vim.cmd("MarkdownPreview")
+      cb({})
+      return
+    end
+
+    if ft == "python" and vim.fn.exists(":CellRun") == 2 then
+      vim.cmd("CellRun")
+      cb({})
+      return
+    end
+
     local cmds = {
-      python = { "python3", file },
-      sh = { "bash", file },
-      lua = { "lua", file },
-      cpp = { "bash", "-c", "g++ " .. file .. " -o /tmp/a.out && /tmp/a.out" },
       c = { "bash", "-c", "gcc " .. file .. " -o /tmp/a.out && /tmp/a.out" },
+      cpp = { "bash", "-c", "g++ " .. file .. " -o /tmp/a.out && /tmp/a.out" },
+      lua = { "lua", file },
+      python = { "python", file },
+      sh = { "bash", file },
+
     }
 
     local cmd = cmds[ft]
